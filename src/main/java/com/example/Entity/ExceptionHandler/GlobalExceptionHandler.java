11a992c16b6d48d5de0d2d9062ad4794e1cc90df
RenaldoNika <roniii2017@icloud.com>
2024-11-15 15:40:26 +0100

    first commit 0.1

--- src/main/java/com/example/Entity/ExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/Entity/ExceptionHandler/GlobalExceptionHandler.java
@@ -2,18 +2,21 @@ package com.example.Entity.ExceptionHandler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(PersonNotFoundException.class)
-    public ResponseEntity<String>hanldlePersonNotFound(PersonNotFoundException ex){
-        return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+    public ResponseEntity<String> handlePersonNotFoundException(PersonNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
     }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGlobalException(Exception ex) {
